03a1ef6233e9516f55572bfb966f56d1438b6cef
Louis <clementmagnus@gmail.com>
2020-03-25 12:23:23 +0100

    handle http requests

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/submit', methods=['POST'])
+def post():
+    data = request.json
+    try:
+        with open("data/test") as f:
+            f.write(data)
+            f.close()
+            return "Success"
+    except:
+        return "Error"
 
 
 if __name__ == "__main__":
